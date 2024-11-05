d0f684eee43189d826280fc1096011f852f329f8
fishstiicks <loketteshimmer@gmail.com>
2024-11-05 14:36:49 -0600

    Add deletes for  review, review image, and booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,12 +1,44 @@
 const express = require('express');
+const { Booking, Sequelize } = require('../../db/models');
+const { Spot } = require('../../db/models');
+const { User } = require('../../db/models');
 const router = express.Router();
 
 // Edit booking by ID
-router.get('/:bookingId', (req, res) => {
+router.get('/:bookingId', async (req, res) => {
 })
 
 // Delete booking by ID
-router.delete('/:bookingId', (req, res) => {
+router.delete('/:bookingId', async (req, res) => {
+    // Authenticate
+    if (!req.user) {
+        return res.status(401).json({ "message": 'Authentification required' });
+    }
+
+    const { bookingId } = req.params;
+    const { booking } = await Booking.findbyPk(bookingId);
+    const spot = await Spot.findbyPk(booking.spotId);
+
+    if (booking.userId !== req.user.id && spot.ownerId !== req.user) {
+        return res.status(403).json({ "message": "Forbidden" });
+    }
+
+    // Construct
+    if (!booking) {
+        return res.status(404).json({
+           "message": "Booking couldn't be found"
+        })
+    }
+
+    if (booking.startDate >= Sequelize.literal('CURRENT TIMESTAMP')) {
+        return res.status(403).json({ "message": "Bookings that have been started can't be deleted"})
+    }
+
+    await Booking.destroy({
+        where: { id: bookingId }
+    });
+
+    return res.status(200).json({ "message": 'Successfully deleted' });
 })
 
 module.exports = router;
--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -9,18 +9,20 @@ router.get('/:reviewId', async (req, res) => {
 
 // Add image to review by ID
 router.post('/:reviewId', async (req, res) => {
+    // Authenticate
     if (!req.user) {
-        return res.status(401).json({ message: 'Authentification required' });
+        return res.status(401).json({ "message": 'Authentification required' });
     }
 
     const { reviewId } = req.params;
-    const { url } = req.body;
-    const { review } = await Review.findOne({where: {id:reviewId}});
+    const { review } = await Review.findbyPk(reviewId);
 
     if (review.userId !== req.user.id) {
-        return res.status(403).json({ message: "Forbidden" });
+        return res.status(403).json({ "message": "Forbidden" });
     }
 
+    // Construct
+    const { url } = req.body;
     const reviewImage = await reviewImage.create({ reviewId: reviewId, url });
 
     const returnReview = {
@@ -31,16 +33,81 @@ router.post('/:reviewId', async (req, res) => {
     return res.status(201).json({review: returnReview})
 })
 
+
 // Edit review by ID
 router.put('/:reviewId', async (req, res) => {
+    // Authenticate
+    if (!req.user) {
+        return res.status(401).json({ "message": 'Authentification required' });
+    }
+
+    const { reviewId } = req.params;
+    const { review } = await Review.findbyPk(reviewId);
+
+    if (review.userId !== req.user.id) {
+        return res.status(403).json({ "message": "Forbidden" });
+    }
+
+    // Construct
 })
+
 
 // Delete review by ID
 router.delete('/:reviewId', async (req, res) => {
+    // Authenticate
+    if (!req.user) {
+        return res.status(401).json({ "message": 'Authentification required' });
+    }
+
+    const { reviewId } = req.params;
+    const { review } = await Review.findbyPk(reviewId);
+
+    if (review.userId !== req.user.id) {
+        return res.status(403).json({ "message": "Forbidden" });
+    }
+
+    // Construct
+    if (!review) {
+        return res.status(404).json({
+           "message": "Review couldn't be found"
+        })
+    }
+
+    await Review.destroy({
+        where: { id: reviewId }
+    });
+
+    return res.status(200).json({ "message": 'Successfully deleted' });
 })
+
 
 // Delete image from review by IDs
 router.delete('/:reviewId/:imageId', async(req, res) => {
+    // Authenticate
+    if (!req.user) {
+        return res.status(401).json({ "message": 'Authentification required' });
+    }
+
+    const { reviewId, imageId } = req.params;
+    const { review } = await Review.findbyPk(reviewId);
+    const { reviewImage } = await reviewImage.findbyPk(reviewId);
+
+    if (review.userId !== req.user.id) {
+        return res.status(403).json({ "message": "Forbidden" });
+    }
+
+    // Construct
+    if (!reviewImage) {
+        return res.status(404).json({
+           "message": "Review Image couldn't be found"
+        })
+    }
+
+    await reviewImage.destroy({
+        where: { id: reviewId, imageId: imageId }
+    });
+
+    return res.status(200).json({ "message": 'Successfully deleted' });
 })
 
 module.exports = router;
