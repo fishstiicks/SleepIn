918950314a6ca85d5c2a3f9d5e2d0d0b73d5f144
Dan4dan4 <danm44@hotmail.com>
2024-11-05 16:13:11 -0500

    Create new spot/ get all spots done + validate

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -87,6 +87,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Spot'
+    
   });
   return Spot;
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const { Spot } = require('../../db/models');
 const router = express.Router();
+const { handleValidationErrors } = require('../../utils/validation');
+const { check } = require('express-validator');
 
-// Create new spot
-router.post('/',
-    //validateCreateSpot,
-    async (req, res) => {
+const validateCreateSpot = [
+    check('address')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Address is required'),
+    check('city')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('City is required'),
+    check('state')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('State is required'),
+    check('country')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Latitude is required'),
+    check('lng')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Longitude is required'),
+    check('name')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Name is required'),
+    check('description')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Description is required'),
+    check('price')
+        .exists({checkFalsy: true})
+        .notEmpty()
+        .withMessage('Price is required'),
+    handleValidationErrors
+  ];
+
+// // Create new spot
+router.post('/', validateCreateSpot, async (req, res, next) => {
         if (!req.user) {
             return res.status(401).json({ message: 'Authentification required' });
         }
@@ -15,6 +55,8 @@ router.post('/',
         const spot = await Spot.create({ ownerId: currentUserId, address, city, state, country, lat, lng, name, description, price });
 
         const returnSpot = {
+            id: spot.id,
+            ownerId: spot.ownerId,
             address: spot.address,
             city: spot.city,
             state: spot.state,
@@ -24,7 +66,10 @@ router.post('/',
             name: spot.name,
             description: spot.description,
             price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
         }
+
 
         return res.status(201).json({spot: returnSpot})
     }
@@ -60,6 +105,7 @@ router.get('/', async (req,res) => {
 
 // Get spots filtered
 router.get('/filtered', async (req,res) => {
+
 })
 
 // Get spot details based on spot ID
@@ -141,6 +187,7 @@ router.post('/:spotId/:imageId', async (req, res) => {
 router.put('/:spotId/reviews', async (req, res) => {
 })
 
+//danish chill dont do these rn 
 // Creates review for spot
 router.post('/:spotId/reviews', async (req, res) => {
 })
@@ -155,3 +202,7 @@ router.post('/:spotId/bookings', async (req, res) => {
 })
 
 module.exports = router;
+
+// module.exports = {
+//     validateCreateSpot
+//   };
